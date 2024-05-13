29f58099a7b829733a0df644452212cbeb3c519d
DevSadit <dev.sadit@gmail.com>
2024-05-13 21:35:07 +0600

    delete wish card from database functionality implemented

--- index.js
+++ index.js
@@ -141,6 +141,14 @@ async function run() {
       res.send(result);
     });
 
+    // deleting the wish card from database
+    app.delete(`/wishlists/:id`, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      // console.log(query);
+      const result = await wishCollection.deleteOne(query);
+      res.send(result);
+    });
     // updating the blog data
     app.put(`/blogss/:id`, async (req, res) => {
       const id = req.params.id;
